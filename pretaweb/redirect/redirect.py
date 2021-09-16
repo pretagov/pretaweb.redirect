bdf3722c129288be4fd462b919e8eb87af108a4b
Jon Pentland <instification@users.noreply.github.com>
2021-09-16 14:40:11 +0100

    Add Python3 compatibility (#1)
    * Make Python3 compatible
    
    * Use last_modified value
    
    * Fix regex to only match on spaces
    
    * use six to provide backwards compatibility with python2
    
    * Check for last_modified attribute before falling back to py2 method
    
    * Add a changelog entry

--- pretaweb/redirect/redirect.py
+++ pretaweb/redirect/redirect.py
@@ -8,18 +8,10 @@ See README.txt for more information.
 
 __version__ = "1.2.1"
 
-from Globals import Persistent, HTMLFile, HTML, MessageDialog, DTMLFile
+from App.special_dtml import HTMLFile, DTMLFile
 from AccessControl.Role import RoleManager
-import Acquisition
-import ExtensionClass
-import sys
-import string
-import OFS
-import Globals
-import time
-import copy
 import re
-import urlparse
+from six.moves.urllib.parse import urlparse
 from AccessControl.SecurityInfo import ClassSecurityInfo
 from OFS.SimpleItem import SimpleItem
 from OFS.ObjectManager import ObjectManager
@@ -96,11 +88,11 @@ class Redirector(ObjectManager):
                 continue
             if line[0] == '=':
                 line = line[1:].strip()
-                parts = re.split(r"(?<!\\)\s*",line) #split on non escaped whitespace
+                parts = re.split(r"(?<!\\)\s+",line) #split on non escaped whitespace
                 if len(parts) == 2:
                     tests.append( parts )
 
-            parts = re.split(r"(?<!\\)\s*",line) #split on non escaped whitespace
+            parts = re.split(r"(?<!\\)\s+",line) #split on non escaped whitespace
             if len(parts) == 2:
                 match, url = parts
                 try:
